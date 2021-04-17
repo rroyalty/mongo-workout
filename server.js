37383236c603d7fc224ad53e49150840c19490cb
rroyalty <rroyalty@gmail.com>
2021-04-17 14:15:11 -0400

    asdf

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
-
-const User = require("./userModel.js");
 
 const app = express();
 
@@ -15,7 +14,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, {   
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
