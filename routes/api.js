a4ee6e52744ce2a393201c8c8e34854325572f21
rroyalty <rroyalty@gmail.com>
2021-04-17 15:03:08 -0400

    heroku test

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const db = require('../models');
+const path = require("path");
 
-router.get()
 
 module.exports = router;
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const apiRoutes = require('./api.js')
+const htmlRoutes = require('./html.js')
+
+// router.use('/api', apiRoutes);
+router.use('/',htmlRoutes);
+
+module.exports = router;
