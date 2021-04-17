37c66c785a0bfb2abcbefa84b012d0682713b2cc
rroyalty <rroyalty@gmail.com>
2021-04-17 14:36:05 -0400

    asdf

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
+const routes = require('./routes')
 const app = express();
 
 app.use(logger("dev"));
@@ -13,6 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+app.use(routes)
 
 mongoose.connect(process.env.MONGODB_URI, {   
   useNewUrlParser: true,
