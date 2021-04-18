4a702226474870921e8b203805c03cf324f7f04a
rroyalty <rroyalty@gmail.com>
2021-04-18 15:26:02 -0400

    small changes

--- routes/api.js
+++ routes/api.js
@@ -3,4 +3,43 @@ const db = require('../models');
 const path = require("path");
 
 
+router.get("/workouts", async (req, res) => {
+    try {
+        const workoutData = await db.Workout.find({})
+        console.log(workoutData)
+        res.status(200).json(workoutData);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    });
+
+router.get("/workouts/range", async (req, res) => {
+    try {
+        const workoutData = await db.Workout.find({})
+        console.log(workoutData)
+        res.status(200).json(workoutData) 
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    });
+
+router.post("/workouts", async (req, res) => {
+    try {
+        const workoutData = await db.Workout.create(req.body)
+        res.status(200).json(workoutData);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    });
+
+router.put("/workouts/:id", async (req, res) => {
+    try{
+        const workoutData = await db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+        res.status(200).json(dbWorkout)
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    });
+
+
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api.js')
 const htmlRoutes = require('./html.js')
 
-// router.use('/api', apiRoutes);
-router.use('/',htmlRoutes);
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
 
 module.exports = router;
